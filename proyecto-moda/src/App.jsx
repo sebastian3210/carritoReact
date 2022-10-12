366b2d94affa719abff31dfc2bcc3cf20af36280
sebastian pastor <sdp.pastor@gmail.com>
2022-10-12 19:10:44 -0300

    first commit

---
+++ proyecto-moda/src/App.jsx
@@ -0,0 +1,27 @@
+
+
+import './App.css'
+import NavBar from './componentes/navBar'
+
+function App() {
+
+
+  
+
+
+  return (
+    
+    <div className="App">
+      <div>
+        <NavBar />
+      </div>
+    </div>
+
+    
+  )
+
+ 
+}
+
+
+export default App
